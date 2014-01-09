e054b41f2c6aa4ea7acd9d44e45574b535db56de
Tom Mullen <tom@gli-students.com>
2014-01-09 13:55:01 +0000

    scope pictures to app

--- app/controllers/ckeditor/pictures_controller.rb
+++ app/controllers/ckeditor/pictures_controller.rb
@@ -1,14 +1,15 @@
 class Ckeditor::PicturesController < Ckeditor::ApplicationController
+  before_filter :load_app
 
   def index
-    @pictures = Ckeditor.picture_adapter.find_all(ckeditor_pictures_scope)
+    @pictures = Ckeditor.picture_adapter.find_all(ckeditor_pictures_scope(app_id: @app.id))
     @pictures = Ckeditor::Paginatable.new(@pictures).page(params[:page])
     
     respond_with(@pictures, :layout => @pictures.first_page?) 
   end
   
   def create
-    @picture = Ckeditor.picture_model.new
+    @picture = Ckeditor.picture_model.new(app_id: @app.id)
 	  respond_with_asset(@picture)
   end
   
@@ -26,5 +27,11 @@ class Ckeditor::PicturesController < Ckeditor::ApplicationController
     def authorize_resource
       model = (@picture || Ckeditor.picture_model)
       @authorization_adapter.try(:authorize, params[:action], model)
+    end
+
+    def load_app
+      @app = current_user.apps.find(session[:app_id])
+      rescue
+        raise 'The App could not be found or you do not have permission to manage it.'
     end
 end
